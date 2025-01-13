a8be8b9e67298b8ed1e13fe01f7b6a70d599da1f
Jack <jacksnape89@gmail.com>
2025-01-13 16:30:01 +0000

    fetched data

--- weathersite/src/app/client.js
+++ weathersite/src/app/client.js
@@ -1,4 +1,26 @@
 import axios from "axios";
 const BASE_URL =
   "https://api.open-meteo.com/v1/forecast?latitude=51.5085,35.6895&longitude=-0.1257,139.6917&hourly=apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,visibility,wind_speed_80m,wind_direction_80m,temperature_80m&timezone=GMT";
-export class ApiClient {}
+export class ApiClient {
+  async responseStatusCheck(responseObject) {
+    if (responseObject.status >= 200 && responseObject.status < 300) {
+      return responseObject.data;
+    }
+    throw new Error(responseObject.statusText);
+  }
+  async getRequest(params = {}) {
+    try {
+      const response = await axios.get(`${BASE_URL}`, { params });
+      return this.responseStatusCheck(response);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  async getLocations() {
+    return this.getRequest("", { results: 10 });
+  }
+  async getWeather({ latitude, longitude }) {
+    return this.getRequest();
+  }
+}
--- weathersite/src/app/page.js
+++ weathersite/src/app/page.js
@@ -3,9 +3,33 @@ import { useState, useEffect } from "react";
 import { ApiClient } from "./client";
 
 export default function Home() {
-  const client = new ApiClient();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const client = new ApiClient(); // Initialize the ApiClient
+  const [loading, setLoading] = useState(true); // State to track loading
+  const [error, setError] = useState(null); // State to track errors
+  const [weatherData, setWeatherData] = useState(null); // State to store weather data
+  const [location, setLocation] = useState("");
+  const fetchData = async () => {
+    try {
+      const data = await client.getWeather({
+        latitude: 51.5085,
+        longitude: -0.1257,
+      });
+      setWeatherData(data);
+      console.log(data);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
 
-  return <div> Hello World</div>;
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Weather Information</h1>
+    </div>
+  );
 }
