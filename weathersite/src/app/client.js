b38d6a70ba860c823ceda606c31b3d5fbd5e638f
Jack <jacksnape89@gmail.com>
2025-01-13 15:29:16 +0000

    latest

---
+++ weathersite/src/app/client.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+const BASE_URL =
+  "https://api.open-meteo.com/v1/forecast?latitude=51.5085,35.6895&longitude=-0.1257,139.6917&hourly=apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,visibility,wind_speed_80m,wind_direction_80m,temperature_80m&timezone=GMT";
+export class ApiClient {}
--- weathersite/src/app/page.js
+++ weathersite/src/app/page.js
@@ -1,7 +1,11 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+import { ApiClient } from "./client";
 
 export default function Home() {
-  return (
-    <div>
-      </div>
-  )}
+  const client = new ApiClient();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  return <div> Hello World</div>;
+}
