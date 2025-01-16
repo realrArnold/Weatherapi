0839f9a984f285147fbe0ec9eca3dbb9583442f5
Ryan Arnold <ryanarnoldcontact@gmail.com>
2025-01-16 10:03:09 +0000

    added-descriptions

--- weathersite/src/app/components/weatherIcons.js
+++ weathersite/src/app/components/weatherIcons.js
@@ -3,11 +3,12 @@ import { weatherData } from "@/app/data/weatherData";
 
 const WeatherIcons = ({ iconKey }) => {
   const selectedIcon = weatherData[iconKey]?.day?.image;
-
+  
+const selectedInfo = weatherData[iconKey]?.day?.description;
   return (
     <div className="flex flex-col items-center p-4 bg-blue-50 shadow-md rounded-md">
       <img src={selectedIcon} className="w-16 h-16 object-contain mb-2" />
-      {/* <p className="text-lg font-medium text-gray-700">{description}</p> */}
+      <p className="text-lg font-medium text-gray-700">{selectedInfo}</p>
     </div>
   );
 };
